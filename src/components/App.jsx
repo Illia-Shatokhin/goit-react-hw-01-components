8d4af97faa66ccae9006d44a2ce2e8b86d7af4f3
Illia Shatokhin <resonane.2106@gmail.com>
2023-06-19 19:55:11 +0300

    first finish homework

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import user from './datas/user.json';
+import data from './datas/data.json';
+import friends from './datas/friends.json';
+import transactions from './datas/transactions.json';
+import { Profile } from './profile/Profile';
+import { Statistics } from './statistics/Statistics';
+import { FriendList } from './friendlist/FriendList';
+import { TransactionHistory } from './transaction-history/TransactionHistory';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
   );
 };
---
+++ src/components/friendlist/FriendList.js
@@ -0,0 +1,44 @@
+import clsx from 'clsx';
+import css from './FriendList.module.css';
+import PropTypes from 'prop-types';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friendList}>
+      {friends.map(friend => {
+        return (
+          <FriendListItem
+            key={friend.id}
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}
+            id={friend.id}
+          />
+        );
+      })}
+    </ul>
+  );
+};
+
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <li className={css.item}>
+      <span
+        className={clsx(css.status, isOnline ? css.online : css.offline)}
+      ></span>
+      <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+      <p className={css.name}>{name}</p>
+    </li>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.exact({
+      avatar: PropTypes.string,
+      name: PropTypes.string,
+      isOnline: PropTypes.bool,
+      id: PropTypes.number,
+    })
+  ),
+};
